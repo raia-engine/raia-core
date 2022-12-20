a9e36f0a847354a3c08bc4e1ed569c9fb4d32458
dolphilia <dolphilia@outlook.com>
2022-12-20 15:57:45 +0900

    update

--- src/raia/raia_duktape.c
+++ src/raia/raia_duktape.c
@@ -34,7 +34,7 @@ static duk_ret_t regist_raia_lib_close_all(duk_context *ctx) {
     return NO_RETURN_VALUE;
 }
 
-static duk_ret_t regist_raia_lib_add(duk_context *ctx) {
+static duk_ret_t regist_raia_lib_addGlobalFunc(duk_context *ctx) {
     const char *dll_func_name = duk_to_string(ctx, 0);
     const char *regist_func_name = duk_to_string(ctx, 1);
     int nargs = (int) duk_to_number(ctx, 2);
@@ -44,27 +44,35 @@ static duk_ret_t regist_raia_lib_add(duk_context *ctx) {
     return NO_RETURN_VALUE;
 }
 
+static duk_ret_t regist_raia_lib_getFunc(duk_context *ctx) {
+    const char *dll_func_name = duk_to_string(ctx, 0);
+    int nargs = (int) duk_to_number(ctx, 1);
+    duk_ret_t (*p_func)(duk_context *ctx) = add_plugin_func(ctx, dll_func_name);
+    duk_push_c_function(ctx, p_func, nargs);
+    return 1;
+}
+
 static void regist_functions(duk_context *ctx) {
     regist_func(ctx, regist_raia_c_puts, "_raia_c_puts", 1);
     regist_func(ctx, regist_raia_lib_init, "_raia_lib_init", 0);
     regist_func(ctx, regist_raia_lib_open, "_raia_lib_open", 1);
     regist_func(ctx, regist_raia_lib_close, "_raia_lib_close", 0);
     regist_func(ctx, regist_raia_lib_close_all, "_raia_lib_close_all", 0);
-    regist_func(ctx, regist_raia_lib_add, "_raia_lib_add", 3);
+    regist_func(ctx, regist_raia_lib_addGlobalFunc, "_raia_lib_add_global_func", 3);
+    regist_func(ctx, regist_raia_lib_getFunc, "_raia_lib_get_func", 2);
 }
 
 static void regist_objects(duk_context *ctx) {
     char *objects =
+            "var print = function(a){_raia_c_puts(a);};"
             "var Raia = {"
-            "    C: {"
-            "        puts: function(a){_raia_c_puts(a);},"
-            "    },"
             "    Lib: {"
             "        init: function(){_raia_lib_init();},"
             "        open: function(a){_raia_lib_open(a);},"
             "        close: function(){_raia_lib_close();},"
             "        closeAll: function(){_raia_lib_close_all();},"
-            "        add: function(a,b,c){_raia_lib_add(a,b,c);},"
+            "        addGlobalFunc: function(a,b,c){_raia_lib_add_global_func(a,b,c);},"
+            "        getFunc: function(a,b){return _raia_lib_get_func(a,b);},"
             "    },"
             "};";
     duk_eval_string(ctx, objects);
