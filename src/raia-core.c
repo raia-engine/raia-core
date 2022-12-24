a321c985c1801193f36a4e8e1830d7892515fcb8
dolphilia <dolphilia@outlook.com>
2022-12-24 11:27:25 +0900

    small update

--- src/raia-core.c
+++ src/raia-core.c
@@ -4,9 +4,9 @@
 
 #include "raia-core.h"
 #include "static/static_arg.h"
+
 int main(int argc, char *argv[]) {
     init_arg(argc, argv);
-
     init_plugin_loader();
     init_duk_ctx();
     duktape_start();
