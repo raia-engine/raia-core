0aa094f69d5259f640575adce2138cb4e73e5d0c
dolphilia <42200736+dolphilia@users.noreply.github.com>
2022-12-31 20:10:06 +0900

    Update raia_duktape.c

--- src/raia/raia_duktape.c
+++ src/raia/raia_duktape.c
@@ -55,7 +55,6 @@ static duk_ret_t regist_raia_context_init(duk_context *ctx) {
     char json[500];
     char* none = "";
 
-
     //replace_char(path_exe, '\\', '/');
     //replace_char(argv_joined, '\\', '/');
 
